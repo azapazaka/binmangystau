c51c6e69f735bd9f1574f5546d00de4131fc7196
Alokkolala <alokkolala@gmail.com>
2026-06-11 19:50:58 +0500

    fix: raise modal z-index above Leaflet map controls

--- src/pages/admin/AdminMapPage.tsx
+++ src/pages/admin/AdminMapPage.tsx
@@ -98,7 +98,7 @@ function ReportModal({
 
   return createPortal(
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4"
+      className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 p-4"
       onClick={onClose}
     >
       <div
